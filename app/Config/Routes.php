df6bba95b49e3d1ac0309b94ee7ed9b2a1207f7a
Hotiana <rasamizafyhotiana@gmail.com>
2026-04-28 09:14:52 +0300

    route login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'User::showlogin');
+$routes->post('/login', 'User::loginUser');
 $routes->get('/register', 'User::showregister');
 $routes->get('/home', 'Home::showhome');
